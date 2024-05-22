78041cfb919770fc5842fa762b6057892db7cc3e
Gautam Mehani <133370824+mehanig001@users.noreply.github.com>
2024-05-23 00:10:17 +0530

    Time: 17 ms (25.66%), Space: 15.9 MB (80.53%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -24,22 +24,40 @@ bool getPath(TreeNode* root, TreeNode* target, vector<TreeNode*> &v){
     return false;
     
 }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*>  v1;
-        vector<TreeNode*>  v2;
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 
-        getPath(root,p,v1);
-        getPath(root,q,v2);
+        if(root == NULL)return NULL;
 
-        unordered_set<TreeNode*> s(v1.begin(),v1.end());
-    
-        for(int i = v2.size()-1; i >=0; i--){
-            if(s.find(v2[i]) != s.end())return v2[i];
-        }
+        if(root == p || root == q)return root;
+
+        if(root->left == NULL && root->right == NULL)return NULL;
+
+        // if(root->left)
+        TreeNode* l = lowestCommonAncestor(root->left,p,q);
+        // if(root->right)
+        TreeNode* r = lowestCommonAncestor(root->right,p,q);
+
+        
+        if(l == r)return l;
+        if(l&&r)return root;
+
+        if(l != NULL)return l;
+        if(r != NULL)return r;
 
         return root;
+        // vector<TreeNode*>  v1;
+        // vector<TreeNode*>  v2;
 
+        // getPath(root,p,v1);
+        // getPath(root,q,v2);
 
+        // unordered_set<TreeNode*> s(v1.begin(),v1.end());
+    
+        // for(int i = v2.size()-1; i >=0; i--){
+        //     if(s.find(v2[i]) != s.end())return v2[i];
+        // }
+
+        // return root;
 
 
 
