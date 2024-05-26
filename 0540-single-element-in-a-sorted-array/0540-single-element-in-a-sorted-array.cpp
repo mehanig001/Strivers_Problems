e3ee4a4b81f32ca1f9cfc4de71bebe526a3ccdea
Gautam Mehani <133370824+mehanig001@users.noreply.github.com>
2024-05-26 14:36:44 +0530

    Time: 17 ms (73.72%), Space: 24.8 MB (5.05%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0; for(auto v : nums)ans = ans ^ v; return ans;
+        if(nums.size() == 1)return nums[0];
+        //sorted ka use nhi kiya kyaa 
+        // first occ even and second odd 
+        int si = 0, ei = nums.size()-1;
+
+        while(si <= ei){
+            int mid = si + (ei-si)/2;
+            bool parity = mid%2;
+            if(1){
+                if(parity == 1 && mid <= nums.size()-2 && nums[mid+1] == nums[mid]){
+                    ei = mid-1;
+                }
+                else if(parity == 1 && mid >= 1 && nums[mid] == nums[mid-1]){
+                    si = mid+1;
+                }
+                else if(parity == 0 && mid <= nums.size()-2 && nums[mid] == nums[mid+1]){
+                    si = mid+2;
+                }
+                else if(parity == 0 && mid >= 1 && nums[mid] == nums[mid-1]){
+                    ei = mid-1;
+                }
+                else{
+                    return nums[mid];
+                }
+            }
+
+        }
+        return -1;
     }
 };
