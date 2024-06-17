30b94a8759e5cc59ed2df9efd552ab3e17bd42d7
Gautam Mehani <133370824+mehanig001@users.noreply.github.com>
2024-06-17 15:54:00 +0530

    Time: 2 ms (82.63%), Space: 9.1 MB (94.27%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -16,25 +16,34 @@ private:
         }
 
         r = i+1;
-        c = col-1;
-        while(r < n && c >= 0){
+        c = col+1;
+        while(r < n && c < n){
             if(board[r][c] == 'Q')return false;
             r++;
-            c--;
+            c++;
         }
 
         return true;
 
     }
-    void nQueen(vector<vector<string>> &ans, vector<string> &board, int col, int n){
+    bool isSafe2(vector<string> &board, int i, int col, int n, vector<int> &leftRow, vector<int> &leftUpper, vector<int> &leftLower){
+        //using short tricks hashing and formula to chk leftUpper and leftLower Diagonals
+
+        if(leftRow[i] || leftLower[i+col] || leftUpper[n-1+col-i] )return false;
+        return true;
+
+    }
+    void nQueen(vector<vector<string>> &ans, vector<string> &board, int col, int n, vector<int> &leftRow, vector<int> &leftUpper, vector<int> &leftLower){
         if(col == n){
             ans.push_back(board);
         }
         for(int i  = 0; i < n; i++){
-            if(isSafe(board,i,col,n)){
+            if(isSafe2(board,i,col,n, leftRow, leftUpper, leftLower)){
                 board[i][col] = 'Q';
-                nQueen(ans,board,col+1, n);
+                leftRow[i] = 1; leftLower[i+col] = 1; leftUpper[n-1+col-i] = 1;
+                nQueen(ans,board,col+1, n, leftRow, leftUpper, leftLower);
                 board[i][col] = '.';
+                leftRow[i] = 0; leftLower[i+col] = 0; leftUpper[n-1+col-i] = 0;
             }
         }
     }
@@ -44,8 +53,9 @@ public:
 
         string x(n,'.');
         vector<string> board(n,x);
-
-        nQueen(ans, board, 0, n);
+        
+        vector<int> leftRow(n,0), leftUpper(2*n-1,0), leftLower(2*n-1,0);
+        nQueen(ans, board, 0, n, leftRow, leftUpper, leftLower);
 
         return ans;
     }
