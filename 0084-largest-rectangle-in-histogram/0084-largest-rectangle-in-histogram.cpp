58df3952a6016c188a34e42e13b4253761812a4f
Gautam Mehani <133370824+mehanig001@users.noreply.github.com>
2024-07-21 17:55:20 +0530

    Time: 102 ms (84.46%), Space: 80.1 MB (69.41%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,65 +1,26 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> leftSmaller(n);
-        vector<int> rightSmaller(n);
-
-        leftSmaller[0] = -1;
         stack<int> st;
-        st.push(0);
-        for(int i = 1; i < n; i++){
-
-            while(!st.empty() && heights[st.top()] >= heights[i]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                leftSmaller[i] = -1;
-            }
-            else{
-                leftSmaller[i] = st.top();
-            }
-            
-            st.push(i);
-        }
-
-        rightSmaller[n-1] = n;
-        while(!st.empty())st.pop();
-
-        st.push(n-1);
-        for(int i = n-2; i >= 0; i--){
-            while(!st.empty() && heights[st.top()] >= heights[i]){
-                st.pop();
-            }
-
-            if(st.empty()){
-                rightSmaller[i] = n;
-            }
-            else{
-                rightSmaller[i] = st.top();
-            }
-            
-            st.push(i);
-        }
-
-        for(auto val : leftSmaller)cout<<val<<" ";cout<<endl;
-        for(auto val : rightSmaller)cout<<val<<" ";cout<<endl;
-
-        
+        int n = heights.size();
         int maxi = 0;
-
         for(int i = 0; i < n; i++){
-            int curr = (rightSmaller[i]-1 - (leftSmaller[i]+1) + 1) * heights[i];
-            maxi = max(maxi, curr);
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                int idx = st.top();
+                st.pop();
+                int curr = (heights[idx])* ((st.empty()) ? i : (i - st.top() - 1));
+                maxi = max(maxi,curr);
+            }
+            st.push(i);
         }
 
+        while(!st.empty()){
+            int idx = st.top();
+            st.pop();
+            int curr = (heights[idx])* ((st.empty()) ? n : (n - st.top() - 1));
+            maxi = max(maxi,curr);
+        }
 
         return maxi;
-        
-
-
-       
-
     }
 };
