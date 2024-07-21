e7fa72a90c39366e6df490b61e207808885e10c1
Gautam Mehani <133370824+mehanig001@users.noreply.github.com>
2024-07-21 23:14:26 +0530

    Time: 3 ms (84.14%), Space: 16.6 MB (17.41%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,55 +1,56 @@
 class Solution {
-private:
-    void bfs(vector<vector<int>>& adj,int n, int m, int &time, queue<pair<int,int>> &q){
- 
-      
-        while(!q.empty()){
-            int N = q.size();
-            int deltaRow[] = {-1,0,1,0};
-            int deltaCol[] = {0,1,0,-1};
-            for(int j = 0; j < N; j++){
-                pair<int,int> tp = q.front();
-                q.pop();
-                int row = tp.first, col = tp.second;
-                // cout<<row<<" "<<col<<endl;
-                for(int i = 0; i < 4; i++){
-                    int newrow = row + deltaRow[i];
-                    int newcol = col + deltaCol[i];
-                    if(newrow < 0 || newrow > n-1 || newcol < 0 || newcol > m-1){
-                        continue;
-                    }
-                    if(adj[newrow][newcol] == 1){
-                        adj[newrow][newcol] = 2;
-                        q.push({newrow,newcol});
-                    }
-                }
-            }
-            time++;
-        }
-    }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int time = 0;
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> adj = grid;
-        queue<pair<int, int>> q;
+
+        queue<pair<int,int>> q;
+        vector<int> visited(n*m,0);
+        int ones = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(adj[i][j] == 2){  
+                if(grid[i][j] == 2){
                     q.push({i,j});
+                    int node = i*m+j;
+                    visited[node] = 1;
                 }
+                else if(grid[i][j] == 1)ones++;
             }
         }
 
-        bfs(adj, n, m, time, q);
 
-        for(auto vc : adj){
-            for(auto val :vc){
-                if(val == 1)return -1;
+        while(!q.empty()){
+            int N = q.size();
+            for(int i = 0; i < N; i++){
+                auto pr = q.front();
+                q.pop();
+
+                int row = pr.first;
+                int col = pr.second;
+
+                int delr[] = {-1,0,1,0};
+                int delc[] = {0,1,0,-1};
+
+                for(int k = 0; k < 4; k++){
+                    int newr = row + delr[k];
+                    int newc = col + delc[k];
+
+                    if(newr >= 0 && newr < n && newc >= 0 && newc < m && grid[newr][newc] == 1 && !visited[newr*m+newc]){
+                        q.push({newr,newc});
+                        int node = newr * m + newc;
+                        visited[node] = 1;
+                        ones--;
+                    }
+                }
+
             }
-        }
+            time++;
 
+        }
+        if(ones != 0){
+            return -1;
+        }
         return max(0,time-1);
     }
 };
