39e547c50f8df4cc377da12180ca3a4ae95465be
Gautam Mehani <133370824+mehanig001@users.noreply.github.com>
2024-08-11 03:36:39 +0530

    Time: 0 ms (100%), Space: 16.6 MB (26.7%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,56 +1,50 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int time = 0;
         int n = grid.size();
         int m = grid[0].size();
 
         queue<pair<int,int>> q;
-        vector<int> visited(n*m,0);
-        int ones = 0;
+        vector<int> vis(n*m,0);
+        int ct = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(grid[i][j] == 2){
                     q.push({i,j});
-                    int node = i*m+j;
-                    visited[node] = 1;
+                    vis[i*m + j] = 1;
                 }
-                else if(grid[i][j] == 1)ones++;
+                else if(grid[i][j] == 1)ct++;
             }
         }
+        
 
+        int delr[] = {-1,0,1,0};
+        int delc[] = {0,1,0,-1};
 
+        int time = 0;
         while(!q.empty()){
             int N = q.size();
-            for(int i = 0; i < N; i++){
+            for(int it = 0; it < N; it++){
                 auto pr = q.front();
                 q.pop();
-
-                int row = pr.first;
-                int col = pr.second;
-
-                int delr[] = {-1,0,1,0};
-                int delc[] = {0,1,0,-1};
-
-                for(int k = 0; k < 4; k++){
-                    int newr = row + delr[k];
-                    int newc = col + delc[k];
-
-                    if(newr >= 0 && newr < n && newc >= 0 && newc < m && grid[newr][newc] == 1 && !visited[newr*m+newc]){
-                        q.push({newr,newc});
-                        int node = newr * m + newc;
-                        visited[node] = 1;
-                        ones--;
+                int r = pr.first;
+                int c = pr.second;
+                for(int i = 0; i < 4; i++){
+                    int nr = r+delr[i];
+                    int nc = c+delc[i];
+                    if(nr >= 0 && nr < n && nc >= 0 && nc < m){
+                        if(grid[nr][nc] == 1 && !vis[nr*m+nc]){
+                            ct--;
+                            vis[nr*m+nc] = 1;
+                            q.push({nr,nc});
+                        }
                     }
                 }
-
             }
             time++;
-
         }
-        if(ones != 0){
-            return -1;
-        }
+        // cout<<ct<<endl;
+        if(ct != 0)return -1;
         return max(0,time-1);
     }
 };
