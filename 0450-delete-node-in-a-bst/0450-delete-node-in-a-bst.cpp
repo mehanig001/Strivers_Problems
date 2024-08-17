aa4bcd46c5eb1dc7c81c60534f39d77b6267f522
Gautam Mehani <133370824+mehanig001@users.noreply.github.com>
2024-08-17 23:58:12 +0530

    Time: 27 ms (37.68%), Space: 33.5 MB (15.61%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -9,51 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    TreeNode* helper(TreeNode* root){
-        if(root == NULL)return NULL;
-        
-        if(root->left == NULL && root->right == NULL)return NULL;
-        if(root->left == NULL){
-            return root->right;
-        }
-        else if(root->right == NULL)return root->left;
-        else{
-            TreeNode* LST = root->left;
-            TreeNode* i = LST;
-            while(i->right)i = i->right;
-            i->right = root->right;
-            root->right = NULL;
-            return LST;
-        }
 
-        
+
+class Solution {
+
+private:
+    TreeNode* helper(TreeNode *temp){
+        TreeNode *LST = temp->left;
+        if(LST == NULL){
+            return temp->right; 
+        }
+        TreeNode* i = LST;
+        while(i && i->right)i = i->right;
+        i->right = temp->right;
+        temp->right = NULL;
+        return temp->left;
     }
-    void searchAndDel(TreeNode* root, int key){
+    void searchAndDel(TreeNode *root, int key){
         if(root == NULL)return;
-        // cout<<root->val<<" ";
         if(root->left && root->left->val == key){
             root->left = helper(root->left);
             return;
         }
         else if(root->right && root->right->val == key){
             root->right = helper(root->right);
-            // cout<<root->val<<" "<<endl;
             return;
-           
         }
-        if(root->val > key)searchAndDel(root->left,key);
-        else searchAndDel(root->right,key);
-        
+        else{
+            if(root->val > key)searchAndDel(root->left, key);
+            else searchAndDel(root->right, key);
+        }
     }
-
+public:
     TreeNode* deleteNode(TreeNode* root, int key) {
         if(root == NULL)return NULL;
         if(root->val == key)return helper(root);
-        searchAndDel(root,key);
 
+        searchAndDel(root, key);
         return root;
-
     }
 };
