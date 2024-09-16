d316f5626083809462402a7ea6a26193952b27ad
Gautam Mehani <133370824+mehanig001@users.noreply.github.com>
2024-09-17 03:19:41 +0530

    Time: 634 ms (5.01%), Space: 288.3 MB (5.57%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,46 +1,25 @@
 class Solution {
-
-int f(int i, int buy, int cap, vector<int> &prices, vector<vector<vector<int>>> &dp){
-    if(i == prices.size())return 0;
-    if(cap == 0)return 0;
-
-    if(dp[i][buy][cap] != -1)return dp[i][buy][cap];
-
-    if(buy){
-        return dp[i][buy][cap] = max(f(i+1,1,cap, prices, dp), -1*prices[i] + f(i+1, 0, cap, prices, dp));
-    }
-    else{
-        return dp[i][buy][cap] = max(f(i+1,0,cap, prices, dp), prices[i] + f(i+1,1,cap-1, prices, dp));
-    }
-}
-
-public:
-     int maxProfit(vector<int>& prices){
-        // Write your code here.
-        //just like finding 1st maximum and second maximum 
-        // return f(0,1,2,prices, dp);
-        int n = prices.size();
-        // vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2, vector<int> (3,0)));
-        vector<vector<int>> after(2, vector<int> (3,0));
-        vector<vector<int>> curr(2, vector<int> (3,0));
-
-        for(int i = n-1; i >= 0; i--){
-            for(int buy = 0; buy <= 1; buy++){
-                for(int cap = 2; cap > 0; cap--){
-
-                    if(buy){
-                        curr[buy][cap] = max(after[1][cap], -1*prices[i] + after[0][cap]);
-                    }
-                    else{
-                        curr[buy][cap] = max(after[0][cap], prices[i] + after[1][cap-1]);
-                    }    
-                }
-            }
-            after = curr;
+    int f(vector<int> &prices, int i, int k, int buy, vector<vector<vector<int>>> &dp){
+        if(k == 0)return 0;
+        if(i == prices.size()-1){
+            if(buy == 0)return 0;
+            else return prices.back();
         }
+        if(dp[i][k][buy] != -1)return dp[i][k][buy];
 
-        return after[1][2];
-        
+        if(buy == 1){
+            int sell = prices[i] + f(prices, i+1, k-1, 0, dp);
+            int notSell = f(prices, i+1, k, 1, dp);
+            return dp[i][k][buy] =  max(sell, notSell);
+        }
+        int by = -1*prices[i] + f(prices, i+1, k, 1, dp);
+        int notBuy = f(prices, i+1, k, 0, dp);
+        return dp[i][k][buy] = max(by, notBuy);
     }
-
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (3, vector<int> (2, -1)));
+        return f(prices, 0, 2, 0, dp);
+    }
 };
